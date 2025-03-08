d5b485b8426d1ce60f8dea2aa2529e4af468b339
jmnuf <jose.m.nufio.0@gmail.com>
2025-03-08 14:48:51 +0100

    [BATMAN] Init vite-pwa project

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'dumb-do-not',
+      short_name: 'ddn',
+      description: 'A stupidly simple todo app for managing minor notes and todos',
+      theme_color: '#181818',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
