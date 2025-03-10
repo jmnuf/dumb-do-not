a15c303a6550abcca9adb94a5ccc1c9b66d0db98
jmnuf <jose.m.nufio.0@gmail.com>
2025-03-10 02:26:21 +0100

    [deps,css] Added tailwind to the dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,39 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
+import { VitePWA } from "vite-plugin-pwa";
+import { tailwindcss } from "@tailwindcss/vite";
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
+  plugins: [
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'dumb-do-not',
-      short_name: 'ddn',
-      description: 'A stupidly simple todo app for managing minor notes and todos',
-      theme_color: '#181818',
-    },
+      manifest: {
+        name: 'dumb-do-not',
+        short_name: 'ddn',
+        description: 'A stupidly simple todo app for managing minor notes and todos',
+        theme_color: '#181818',
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    })
+  ],
 })
