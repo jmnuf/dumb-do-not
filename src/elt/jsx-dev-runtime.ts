f11567a5e20eee1d596b5c3ba433553aadef42b0
jmnuf <jose.m.nufio.0@gmail.com>
2025-04-19 12:21:31 +0200

    jsx runtime stuff

--- src/elt/jsx-dev-runtime.ts
+++ src/elt/jsx-dev-runtime.ts
@@ -2,4 +2,6 @@ import "./types";
 import { E } from "../E";
 
 export const jsxDEV = E;
+// @ts-expect-error TODO: Need to converge the types appropriately
+export const Fragment = (props: { children?: JSXNode | JSXNode[] | undefined; }) => props.children == null ? Frag() : Frag(props.children);
 
--- src/elt/jsx-runtime.ts
+++ src/elt/jsx-runtime.ts
@@ -1,5 +1,8 @@
-import "./types";
-import { E } from "../E";
+import { JSXNode } from "./types";
+import { E, Frag } from "../E";
 
 export const jsx = E;
+export const jsxs = E;
+// @ts-expect-error TODO: Need to converge the types appropriately
+export const Fragment = (props: { children?: JSXNode | JSXNode[] | undefined; }) => props.children == null ? Frag() : Frag(props.children);
 
